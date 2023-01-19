cb8447cf5f0e8405241f14213df8bff76413f0b9
Laisses <laisse@gmail.com>
2023-01-19 10:40:45 -0300

    create constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+/* export const BASE_URL = ""; */
+
+export const YELLOW = "#F2B705";
+export const DARK_GREY ="#C7D0D8";
+export const LIGTH_GREY = "#F2F2F2";
+export const BLACK = "#0D0D0D";
+
+export const PRIMARY_FONT = "Quicksand', sans-serif";
+export const SECONDARY_FONT = "'Poppins', sans-serif";
