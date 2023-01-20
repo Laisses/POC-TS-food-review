f1e4979aa6061c89fe7391cfb66d8cf2134bbe54
Laisses <laisse@gmail.com>
2023-01-20 17:01:25 -0300

    src/pages/Reviews.js: create my reviews page

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+import { YELLOW, SECONDARY_FONT } from "../constants/constants";
+
 export const Reviews = () => {
-    return <div>Revisa aí</div>
+    return (
+        <MainContainer>
+            <IntroContainer>
+                <Header>
+                    Coming <span>Soon </span>...
+                </Header>
+            </IntroContainer>
+        </MainContainer>
+    );
 };
+
+const MainContainer = styled.div`
+    padding: 0 4rem;
+    display: flex;
+    flex-direction: column;
+`;
+
+const IntroContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Header = styled.h1`
+    font-family: ${SECONDARY_FONT};
+    font-size: 2rem;
+    text-align: center;
+    margin-bottom: .6rem;
+    span {
+        color: ${YELLOW};
+    }
+`;
