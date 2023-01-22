adb923ad7f937d5197d892a668b5c842d21a9ee4
Laisses <laisse@gmail.com>
2023-01-22 14:22:40 -0300

    src/components/Places.js: connect app with database

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,36 +1,60 @@
 import styled from "styled-components";
+import axios from "axios";
 import { ListOfPlaces } from "./ListPlaces";
-
-const places = [
-    {
-        id: 1,
-        name: "Pub",
-        category: "Bar",
-        rating: undefined,
-    },
-    {
-        id: 2,
-        name: "Restaurante1",
-        category: "Restaurante",
-        rating: "ok",
-    }
-];
+import { BASE_URL, YELLOW } from "../constants/constants";
+import { useEffect, useState } from "react";
+import { Form } from "../components/NewPlace";
 
 export const Place = () => {
-    return (
-        <List>
-            <ListTitle>
-                <Item>Name</Item>
-                <Item>Category</Item>
-                <Item>Rating</Item>
-                <Item></Item>
-            </ListTitle>
-            {places.map(p => <ListOfPlaces
-                key={p.id}
-                {...p}
-            />)}
-        </List>
-    );
+    const [places, setPlaces] = useState(undefined);
+    const [openForm, setOpenForm] = useState(false);
+
+
+    useEffect(() => {
+        getPlaces();
+    }, []);
+
+    const getPlaces = async () => {
+        try {
+            const res = await axios.get(`${BASE_URL}/`);
+            setPlaces(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    if (!places) {
+        return (
+            <NoItemMessage>Loading...</NoItemMessage>
+        );
+    } else if (places.length === 0) {
+        return (
+            <NoItemMessage>You hanven't added a place yet</NoItemMessage>
+        );
+    } else if (places.length > 0) {
+        return (
+            <>
+                <List>
+                    <ListTitle>
+                        <Item>Name</Item>
+                        <Item>Category</Item>
+                        <Item>Rating</Item>
+                        <Item></Item>
+                    </ListTitle>
+                    {places.map(p => <ListOfPlaces
+                        key={p.id}
+                        {...p}
+                    />)}
+                </List>
+                <Button
+                    onClick={() => setOpenForm(true)}
+                >
+                    add new place
+                </Button>
+                {openForm && <Form setOpenForm={setOpenForm} />}
+            </>
+        );
+    }
 };
 
 const List = styled.ul`
@@ -51,4 +75,21 @@ const Item = styled.div`
     min-width: 8rem;
     padding: .8rem 0;
     display: flex;
+`;
+
+const NoItemMessage = styled.h2`
+    margin: 4rem;
+`;
+
+const Button = styled.button`
+    width: 10rem;
+    height: 2rem;
+    color: #fcfcfc;
+    background-color: ${YELLOW};
+    border: none;
+    border-radius: .8rem;
+    box-shadow: 0px 2px 15px rgba(0,0,0,0.2);
+    &:active {
+        transform: translateY(1px);
+    }
 `;
--- src/components/Places.js
+++ src/components/Places.js
@@ -1,24 +1,14 @@
 import styled from "styled-components";
-import { useState } from "react";
 import { device } from "../constants/device";
 import { Place } from "./Place";
-import { Form } from "../components/NewPlace";
-import { YELLOW } from "../constants/constants";
 
 export const Places = () => {
-    const [openForm, setOpenForm] = useState(false);
     return (
         <PlacesContainer>
             <Title>
                 My places
             </Title>
             <Place />
-            <Button
-                onClick={() => setOpenForm(true)}
-            >
-                add new place
-            </Button>
-            {openForm && <Form setOpenForm={setOpenForm} />}
         </PlacesContainer>
     );
 };
@@ -34,17 +24,4 @@ const PlacesContainer = styled.div`
 const Title = styled.h2`
     font-size: 1.5rem;
     margin-bottom: 1.3rem;
-`;
-
-const Button = styled.button`
-    width: 10rem;
-    height: 2rem;
-    color: #fcfcfc;
-    background-color: ${YELLOW};
-    border: none;
-    border-radius: .8rem;
-    box-shadow: 0px 2px 15px rgba(0,0,0,0.2);
-    &:active {
-        transform: translateY(1px);
-    }
 `;
