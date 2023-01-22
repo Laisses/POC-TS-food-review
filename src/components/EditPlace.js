2545120709be291a8cc4ed98297d68cb983a808a
Laisses <laisse@gmail.com>
2023-01-22 14:20:08 -0300

    src/components/EditPlace.js: connect edit button with database

--- src/components/EditPlace.js
+++ src/components/EditPlace.js
@@ -1,22 +1,33 @@
 import styled from "styled-components";
 import { useState } from "react";
-import { DARK_GREY, PRIMARY_FONT } from "../constants/constants";
+import { DARK_GREY, PRIMARY_FONT, BASE_URL, LIGHT_GREY } from "../constants/constants";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { device } from "../constants/device";
+import axios from "axios";
 
 export const EditForm = props => {
-    const {name, category, setEdit} = props;
-    const [form, setForm] = useState({name, category});
+    const { id, name, category, setEdit } = props;
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({ name, category });
 
     const handleForm = e => {
         const { name, value } = e.target;
         setForm({ ...form, [name]: value });
     };
 
-    const submitForm = () => {
-        console.log("mandou a mudança");
-        setEdit(false);
+    const submitForm = async () => {
+        setLoading(true);
+
+        try {
+            await axios.put(`${BASE_URL}/${id}`, form);
+            window.location.reload();
+            setLoading(false);
+            setEdit(false);
+        } catch (err) {
+            console.log(err.response.data);
+            setLoading(false);
+        }
     };
 
     return (
@@ -27,18 +38,26 @@ export const EditForm = props => {
                     name="name"
                     value={form.name}
                     onChange={e => handleForm(e)}
+                    disabled={loading}
                 />
                 <TextInput
                     id="category"
                     name="category"
                     value={form.category}
                     onChange={e => handleForm(e)}
+                    disabled={loading}
                 />
             </TextContainer>
-            <EditIcons onClick={() => submitForm()}>
+            <EditIcons
+                onClick={() => submitForm()}
+                disabled={loading}
+            >
                 <FontAwesomeIcon icon={faCircleCheck} color="#34A621" />
             </EditIcons>
-            <EditIcons onClick={() => setEdit(false)}>
+            <EditIcons
+                onClick={() => setEdit(false)}
+                disabled={loading}
+            >
                 <FontAwesomeIcon icon={faCircleXmark} color="#D90D0D" />
             </EditIcons>
         </EditFormContainer>
@@ -56,6 +75,7 @@ const TextInput = styled.input`
     border: 1px solid ${DARK_GREY};
     border-radius: 3px;
     font-family: ${PRIMARY_FONT};
+    background-color: ${props => !props.disabled ? "#ffffff" : LIGHT_GREY};
     max-width: 7.4rem;
     height: 2rem;
     display: flex;
